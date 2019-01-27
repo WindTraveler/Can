5bc3d3763be42c8133103b6fa87cd7e2e83a7b2e
WindTraveler <13301156@bjtu.edu.cn>
2019-01-27 18:41:22 +0800

    feat: 响应式数组属性

--- src/can/observer/index.js
+++ src/can/observer/index.js
@@ -1,5 +1,4 @@
 
-
 /**
  * 给对象的一个属性定义响应式属性
  */
@@ -21,7 +20,7 @@ function defineReactive(obj, key, val, cb) {
                 return ;
             }
             val = newVal;
-            cb();
+            typeof cb === "function" && cb();
         }
     })
 }
@@ -29,9 +28,91 @@ function defineReactive(obj, key, val, cb) {
 
 
 export function observe(value, cb) {
-    if (value && typeof value === 'object') {
-        Object.keys(value).forEach(key => {
-            defineReactive(value, key, value[key], cb)
+    if (Array.isArray(value)) {
+        // 数组本身
+        value.cb = cb;
+
+        protoAugment(value, arrayMethods);
+
+        // 数组项
+        observeArray(value, cb);
+    }
+    else {
+        // 走对象
+        walk(value, cb);
+    }
+}
+
+
+function observeArray (items, cb) {
+    for (var i = 0, l = items.length; i < l; i++) {
+        observe(items[i], cb);
+    }
+}
+
+function walk(obj, cb) {
+    if (obj && typeof obj === 'object') {
+        Object.keys(obj).forEach(key => {
+            defineReactive(obj, key, obj[key], cb);
         });
     }
 }
+
+
+function protoAugment (target, src) {
+    /* eslint-disable no-proto */
+    target.__proto__ = src;
+    /* eslint-enable no-proto */
+}
+
+function def (obj, key, val, enumerable) {
+    Object.defineProperty(obj, key, {
+        value: val,
+        enumerable: !!enumerable,
+        writable: true,
+        configurable: true
+    });
+}
+
+var arrayProto = Array.prototype;
+var arrayMethods = Object.create(arrayProto);
+
+var methodsToPatch = [
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'splice',
+    // 'sort',
+    // 'reverse'
+];
+
+/**
+ * Intercept mutating methods and emit events
+ */
+methodsToPatch.forEach(function (method) {
+    // cache original method
+    var original = arrayProto[method];
+    def(arrayMethods, method, function mutator () {
+        var args = [], len = arguments.length;
+        var cb = this.cb;
+        while ( len-- ) args[ len ] = arguments[ len ];
+
+        var result = original.apply(this, args);
+        // var ob = this.__ob__;
+        var inserted;
+        switch (method) {
+            case 'push':
+            case 'unshift':
+                inserted = args;
+                break
+            case 'splice':
+                inserted = args.slice(2);
+                break
+        }
+        if (inserted) { observeArray(inserted, cb); }
+        // 触发重绘
+        cb();
+        return result;
+    });
+});
